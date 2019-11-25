c12f0a9acfbfa4110ee97de6a66669ea52e82e9a
PriscillaLumor <priscilla.lumor@student.rocva.nl>
2019-11-25 15:01:19 +0100

    opdracht 1

--- conditions_opdrachten/conditions_opdracht_1/js/script.js
+++ conditions_opdrachten/conditions_opdracht_1/js/script.js
@@ -1,1 +1,8 @@
 //javascript code
+function getSaldo(){
+    var mySaldo =10;
+
+    if(mySaldo <= 25){
+        document.getElementById("saldo").innerHTML="Sorry, je hebt te weinig saldo!";
+    }
+}
