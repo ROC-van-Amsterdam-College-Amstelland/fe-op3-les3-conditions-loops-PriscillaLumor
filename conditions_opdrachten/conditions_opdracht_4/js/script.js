0b36a39319aab6f326efed9dd40bef210b5069e4
PriscillaLumor <priscilla.lumor@student.rocva.nl>
2019-12-02 15:04:51 +0100

    opdr 4 goedgedaan

--- conditions_opdrachten/conditions_opdracht_4/js/script.js
+++ conditions_opdrachten/conditions_opdracht_4/js/script.js
@@ -1,1 +1,7 @@
 //javascript code
+function getWaarde () {
+    var myInput = document.getElementById("mijninput").value;
+    document.getElementById("waarde").innerHTML = "De variabele myNumber heeft de waarde:"+ myInput;
+}
+
+
