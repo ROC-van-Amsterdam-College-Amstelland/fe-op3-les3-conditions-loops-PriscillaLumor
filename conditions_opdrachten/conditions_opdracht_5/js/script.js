e1fc1c26c80a07d64666137443cbe7d4a32c3924
PriscillaLumor <priscilla.lumor@student.rocva.nl>
2019-12-05 11:31:23 +0100

    opdr 6

--- conditions_opdrachten/conditions_opdracht_5/js/script.js
+++ conditions_opdrachten/conditions_opdracht_5/js/script.js
@@ -12,17 +12,3 @@ function Aft(){
     result=fst-snd;
     calc.result.value=result;
 }
-
-function Verm(){
-    fst=parseInt(calc.fst.value);
-    snd=parseInt(calc.snd.value);
-    result=fst*snd;
-    calc.result.value=result;
-}
-
-function Verd(){
-    fst=parseInt(calc.fst.value);
-    snd=parseInt(calc.snd.value);
-    result=fst/snd;
-    calc.result.value=result;
-}
--- conditions_opdrachten/conditions_opdracht_6/js/script.js
+++ conditions_opdrachten/conditions_opdracht_6/js/script.js
@@ -1,1 +1,15 @@
 //javascript code
+
+function Verm(){
+    fst=parseInt(calc.fst.value);
+    snd=parseInt(calc.snd.value);
+    result=fst*snd;
+    calc.result.value=result;
+}
+
+function Verd(){
+    fst=parseInt(calc.fst.value);
+    snd=parseInt(calc.snd.value);
+    result=fst/snd;
+    calc.result.value=result;
+} 
