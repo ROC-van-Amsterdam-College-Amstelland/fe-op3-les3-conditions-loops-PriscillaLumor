ca4396affc033bc7893b4fc91bdb35c5bf45e1a2
PriscillaLumor <priscilla.lumor@student.rocva.nl>
2019-12-05 11:22:51 +0100

    opdr 5

--- conditions_opdrachten/conditions_opdracht_5/js/script.js
+++ conditions_opdrachten/conditions_opdracht_5/js/script.js
@@ -1,1 +1,28 @@
 //javascript code
+function Opt(){
+    fst=parseInt(calc.fst.value);
+    snd=parseInt(calc.snd.value);
+    result=fst+snd;
+    calc.result.value=result;
+}
+
+function Aft(){
+    fst=parseInt(calc.fst.value);
+    snd=parseInt(calc.snd.value);
+    result=fst-snd;
+    calc.result.value=result;
+}
+
+function Verm(){
+    fst=parseInt(calc.fst.value);
+    snd=parseInt(calc.snd.value);
+    result=fst*snd;
+    calc.result.value=result;
+}
+
+function Verd(){
+    fst=parseInt(calc.fst.value);
+    snd=parseInt(calc.snd.value);
+    result=fst/snd;
+    calc.result.value=result;
+}
