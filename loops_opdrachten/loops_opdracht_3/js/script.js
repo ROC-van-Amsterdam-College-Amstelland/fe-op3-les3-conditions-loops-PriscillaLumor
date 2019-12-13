f8e9b11e85ff9c8cb63a4de98ef72cde3da9e96f
PriscillaLumor <priscilla.lumor@student.rocva.nl>
2019-12-13 11:44:31 +0100

    opdr 3

--- loops_opdrachten/loops_opdracht_3/js/script.js
+++ loops_opdrachten/loops_opdracht_3/js/script.js
@@ -1,1 +1,14 @@
 //javascript code
+function myFunction()
+{
+var nummer = "";
+
+for (i= 1; i <=50; i++) {
+    nummer += "<br> number is " + i;
+    
+    if(i == 20 || i == 40 ){
+        document.getElementById("myOutput").innerHTML = nummer += " Dit is een mooie getaal";
+}
+document.getElementById("myOutput").innerHTML = nummer;
+}
+}
